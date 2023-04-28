8db7a387587cfd78d0f1549eeb4c68f0d4bc3d48
Svasyah <101178539+Svasyah@users.noreply.github.com>
2023-04-28 18:50:05 +1000

    Add files via upload

---
+++ quiz_game_login.js
@@ -0,0 +1,11 @@
+function addUser(){
+    player1_input=document.getElementById("player1_input").value;
+    player2_input=document.getElementById("player2_input").value;
+    
+    localStorage.setItem("player1_input", player1_input);
+    localStorage.setItem("player2_input", player2_input);
+    
+    window.location="quiz_game_page.html";
+    }
+
+    
